74fd764c1b864b5ca0c7abd482871fe99d99b66f
priyankaramteke <priyankaramteke1506@gmail.com>
2023-05-18 13:54:17 +0530

    body section

---
+++ src/app/body-secton/body-secton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body-secton',
+  templateUrl: './body-secton.component.html',
+  styleUrls: ['./body-secton.component.css']
+})
+export class BodySectonComponent {
+
+}
