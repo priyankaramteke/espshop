00d19ae22fb3b73c48474481bafbcd4566eb29b7
priyankaramteke <priyankaramteke1506@gmail.com>
2023-06-05 10:49:27 +0530

    added in prod

--- src/app/card-section/card-section.component.ts
+++ src/app/card-section/card-section.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ProductService } from '../product.service';
 
 
 @Component({
@@ -10,21 +11,21 @@ import { Router } from '@angular/router';
 export class CardSectionComponent {
   imgCollectionCard: any = [];
   Drinkssection: any = [];
-  nameSearch:string='';
+  nameSearch: string = '';
   quantity = 0;
   slides = [{ img: '' }]
-  constructor(private router:Router) {}
-  
+  constructor(private router: Router, private productService: ProductService) { }
+
   ngOnInit() {
     this.imgCollectionCard = [{ "image": "assets/imgcardsection/1card.png", "name": "Amul Cheese Cubes", "kg": "200g", "rs": "127 Rs", "cartCount": 0 }, { "image": "assets/imgcardsection/2card.png", "name": "Nestle Milkmaid Sweetened Condensed..", "kg": "380g", "rs": "138 Rs", "cartCount": 0 }, { "image": "assets/imgcardsection/3card.png", "name": "Amul Taaza Toned Fresh Milk", "kg": "500ml", "rs": "27 Rs", "cartCount": 0 }, { "image": "assets/imgcardsection/4card.png", "name": "Britannia 100% Whole Wheat Breads", "kg": "400g", "rs": "50 Rs", "cartCount": 0 }, { "image": "assets/imgcardsection/5card.png", "name": "Amul Cheese Cubes", "kg": "200g", "rs": "91 Rs", "cartCount": 0 }, { "image": "assets/imgcardsection/6card.png", "name": "Amul Fresh Malai Paneer", "kg": "200g", "rs": "127 Rs", "cartCount": 0 }, { "image": "assets/imgcardsection/7card.png", "name": "Amul Salted Butter", "kg": "100g", "rs": "56 Rs", "cartCount": 0 }, { "image": "assets/imgcardsection/8card.png", "name": "Amul Cheese Cubes", "kg": "200g", "rs": "127 Rs", "cartCount": 0 }, { "image": "assets/imgcardsection/9card.png", "name": "Amul Cheese Cubes", "kg": "200g", "rs": "127 Rs", "cartCount": 0 }, { "image": "assets/imgcardsection/10card.png", "name": "Amul Cheese Cubes", "kg": "200g", "rs": "127 Rs", "cartCount": 0 }, { "image": "assets/imgcardsection/11card.png", "name": "Amul Cheese Cubes", "kg": "200g", "rs": "127 Rs", "cartCount": 0 }]
 
 
 
-    this.Drinkssection = [{"image":"assets/drinksimgsection/1orang.png","thumbImage":"assets/drinksimgsection/1orang.png","name":"Tropicana 100% Orange Juice","kg":"250ml","rs":"127 Rs","cartCount":0},{"image":"assets/drinksimgsection/2red.png","thumbImage":"assets/drinksimgsection/2red.png","name":"Red Bull Energy Drink (250 ml)","kg":"250ml","rs":"128 Rs","cartCount":0},{"image":"assets/drinksimgsection/3thums.png","thumbImage":"assets/drinksimgsection/3thums.png","name":"Thums Up Soft Drink (750 ml)","kg":"700ml","rs":"75 Rs","cartCount":0},{"image":"assets/drinksimgsection/4maaza.png","thumbImage":"assets/drinksimgsection/4maaza.png","name":"Maaza Mango Drink 1.2 l","kg":"1000ml","rs":"70 Rs","cartCount":0},{"image":"assets/drinksimgsection/5rose.png","thumbImage":"assets/drinksimgsection/5rose.png","name":"Hamdard Rooh Afza Rose Sharbat","kg":"200ml","rs":"91 Rs","cartCount":0},{"image":"assets/drinksimgsection/6realf.png","thumbImage":"assets/drinksimgsection/6realf.png","name":"Real Fruit Power Cranberry Juice","kg":"200ml","rs":"117 Rs","cartCount":0},{"image":"assets/drinksimgsection/7pulpyor.png","thumbImage":"assets/drinksimgsection/7pulpyor.png","name":"Minute Maid Pulpy Orange Fruit Drink","kg":"250ml","rs":"86 Rs","cartCount":0},{"image":"assets/drinksimgsection/8mixedfruit.png","thumbImage":"assets/drinksimgsection/8mixedfruit.png","name":"B Natural Mixed Fruit Juice - Buy 1 Get 1 Free","kg":"1l","rs":"137 Rs","cartCount":0},{"image":"assets/drinksimgsection/9soda.png","thumbImage":"assets/drinksimgsection/9soda.png","name":"Bisleri Soda Water","kg":"750ml","rs":"28 Rs","cartCount":0}]
-   
-     
+    this.Drinkssection = [{ "image": "assets/drinksimgsection/1orang.png", "thumbImage": "assets/drinksimgsection/1orang.png", "name": "Tropicana 100% Orange Juice", "kg": "250ml", "rs": "127 Rs", "cartCount": 0 }, { "image": "assets/drinksimgsection/2red.png", "thumbImage": "assets/drinksimgsection/2red.png", "name": "Red Bull Energy Drink (250 ml)", "kg": "250ml", "rs": "128 Rs", "cartCount": 0 }, { "image": "assets/drinksimgsection/3thums.png", "thumbImage": "assets/drinksimgsection/3thums.png", "name": "Thums Up Soft Drink (750 ml)", "kg": "700ml", "rs": "75 Rs", "cartCount": 0 }, { "image": "assets/drinksimgsection/4maaza.png", "thumbImage": "assets/drinksimgsection/4maaza.png", "name": "Maaza Mango Drink 1.2 l", "kg": "1000ml", "rs": "70 Rs", "cartCount": 0 }, { "image": "assets/drinksimgsection/5rose.png", "thumbImage": "assets/drinksimgsection/5rose.png", "name": "Hamdard Rooh Afza Rose Sharbat", "kg": "200ml", "rs": "91 Rs", "cartCount": 0 }, { "image": "assets/drinksimgsection/6realf.png", "thumbImage": "assets/drinksimgsection/6realf.png", "name": "Real Fruit Power Cranberry Juice", "kg": "200ml", "rs": "117 Rs", "cartCount": 0 }, { "image": "assets/drinksimgsection/7pulpyor.png", "thumbImage": "assets/drinksimgsection/7pulpyor.png", "name": "Minute Maid Pulpy Orange Fruit Drink", "kg": "250ml", "rs": "86 Rs", "cartCount": 0 }, { "image": "assets/drinksimgsection/8mixedfruit.png", "thumbImage": "assets/drinksimgsection/8mixedfruit.png", "name": "B Natural Mixed Fruit Juice - Buy 1 Get 1 Free", "kg": "1l", "rs": "137 Rs", "cartCount": 0 }, { "image": "assets/drinksimgsection/9soda.png", "thumbImage": "assets/drinksimgsection/9soda.png", "name": "Bisleri Soda Water", "kg": "750ml", "rs": "28 Rs", "cartCount": 0 }]
 
-    
+
+
+
     this.Drinkssection = this.Drinkssection.map((r: any) => {
       r['cartCount'] = 0;
       return r;
@@ -54,11 +55,12 @@ export class CardSectionComponent {
 
   slickInit(e: any) {
   }
-  goprodetails(){
+  goprodetails(item: any) {
+    this.productService.mycartproduct = item;
     this.router.navigate(['prodetails'])
   }
 
- 
+
   addCart(category: any) {
     // console.log(category)
   }
@@ -73,13 +75,13 @@ export class CardSectionComponent {
     // console.log(i)
     this.imgCollectionCard[i]['cartCount'] = this.imgCollectionCard[i]['cartCount'] + 1;
   }
-  subtraproducttwo(i:any){
+  subtraproducttwo(i: any) {
     console.log(i)
     if (this.Drinkssection[i]['cartCount'] > 0) {
       this.Drinkssection[i]['cartCount'] = this.Drinkssection[i]['cartCount'] - 1;
     }
   }
-  addproducttwo(i:number){
-this.Drinkssection[i]['cartCount'] =this.Drinkssection[i]['cartCount'] + 1
+  addproducttwo(i: number) {
+    this.Drinkssection[i]['cartCount'] = this.Drinkssection[i]['cartCount'] + 1
   }
 }
