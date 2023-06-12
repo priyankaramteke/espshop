5afbe873db94a9c18259668cc982e8f0041310bb
priyankaramteke <priyankaramteke1506@gmail.com>
2023-06-12 10:47:22 +0530

    login chnages

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,29 +8,64 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  loginform:any= FormGroup;
-  Signin:Boolean=false;
+  loginform: any = FormGroup;
+  registerform: any = FormGroup;
+  Signin: Boolean = false;
+  isLogin = true;
 
-  constructor( private formbuilder:FormBuilder){}
-  ngOnInit(){
+  constructor(private formbuilder: FormBuilder) { }
+  ngOnInit() {
     this.loginform = this.formbuilder.group({
-      email:new FormControl('',[Validators.required]),
-      password: new FormControl('',[Validators.required])
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
     })
+
+    this.registerform = this.formbuilder.group({
+      name: new FormControl('', [Validators.required]),
+      mob: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    })
+     
   }
-  get f(){
+  get f() {
     return this.loginform.controls
   }
-  metadata:any=''
+  metadata: any = ''
 
-  submit(){
-    this.Signin =true;
-    if(this.loginform.valid){
-      this.metadata={
+  submit() {
+    console.log(this.loginform)
+    console.log(this.registerform)
+    this.Signin = true;
+    if (this.loginform.valid) {
+      this.metadata = {
+       
         email: this.loginform.value.email,
         password: this.loginform.value.password,
       }
       console.log(this.loginform.value)
     }
+  }
+
+   get b(){
+    return this.registerform.controls
+   }
+   valuedata:any=''
+
+   Registerdata(){
+    this.Signin=true;
+    if(this.registerform.valid){
+      this.valuedata={
+        name: this.registerform.value.name,
+        mob: this.registerform.value.mob,
+        email: this.registerform.value.email,
+        password: this.registerform.value.password,
+      }
+    }
+   }
+
+
+  registerHandler() {
+    this.isLogin = !this.isLogin;
   }
 }
